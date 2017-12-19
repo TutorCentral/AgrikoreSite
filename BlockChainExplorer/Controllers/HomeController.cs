2aeff4baa1e171fe77ec5c20f7115cb71da7daa0
agrikore <admin@agrikore.net>
2017-12-18 23:25:41 -0500

    Added a blockchain network page, and links to it.

--- BlockChainExplorer/Controllers/HomeController.cs
+++ BlockChainExplorer/Controllers/HomeController.cs
@@ -23,63 +23,70 @@ namespace BlockChainExplorer.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Contact Us.";
+            ViewBag.Message = "Contact Us";
+
+            return View();
+        }
+
+        public ActionResult Network()
+        {
+            ViewBag.Message = "The MULA Network";
 
             return View();
         }
 
         public ActionResult IMESimulation()
         {
-            ViewBag.Message = "Initial MULA Exchange Simulation Page.";
+            ViewBag.Message = "Initial MULA Exchange Simulation Page";
 
             return View();
         }
 
         public ActionResult BlockchainInfo()
         {
-            ViewBag.Message = "MULA Blockchain Information.";
+            ViewBag.Message = "MULA Blockchain Information";
 
             return View();
         }
 
         public ActionResult IMEAdmin()
         {
-            ViewBag.Message = "Initial MULA Exchange Administration.";
+            ViewBag.Message = "Initial MULA Exchange Administration";
 
             return View();
         }
 
         public ActionResult TokenExchange()
         {
-            ViewBag.Message = "MULA/ETH Exchange.";
+            ViewBag.Message = "MULA/ETH Exchange";
 
             return View();
         }
 
         public ActionResult WhitePaper()
         {
-            ViewBag.Message = "The Agrikore White Paper.";
+            ViewBag.Message = "The Agrikore White Paper";
 
             return View();
         }
 
         public ActionResult Team()
         {
-            ViewBag.Message = "The Agrikore Team.";
+            ViewBag.Message = "The Agrikore Team";
 
             return View();
         }
 
         public ActionResult BitcoinBlockExplorer()
         {
-            ViewBag.Message = "Bitcoin Block EXplorer.";
+            ViewBag.Message = "Bitcoin Block EXplorer";
 
             return View();
         }
 
         public ActionResult AboutBitcoinBlockExplorer()
         {
-            ViewBag.Message = "About Bitcoin Block EXplorer.";
+            ViewBag.Message = "About Bitcoin Block EXplorer";
 
             return View();
         }
---
+++ BlockChainExplorer/Controllers/NetworkController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlockChainExplorer.Controllers
+{
+    public class NetworkController : Controller
+    {
+        // GET: Network
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Network()
+        {
+            ViewBag.Message = "The MULA Network Live View";
+
+            return View();
+        }
+    }
+}
